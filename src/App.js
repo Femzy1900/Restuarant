f958e230b78dacd83c3054a957057b952f9683e6
unknown <adedokunfemi14@gmail.com>
2023-07-19 07:43:22 -0700

    Revert "Merge"
    This reverts commit 8f5b30c5e33483a8f2e40267ebc91d91f2c704be, reversing
    changes made to d5eb88f5cdcaa29ff58de19048deeb03f394e538.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React from "react";
+import Main from "./components/main";
+import Navbar from "./components/Navbar";
+
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Main />
-      {/* <Tuts />
-      <State /> */}
     </div>
   );
 }
